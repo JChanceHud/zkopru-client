ffabd36b68ce77be26420c233cc9224ceb03c02d
Chance Hudson <jchancehud@gmail.com>
2020-12-24 02:23:20 +0000

    functional coordinator

--- coordinator/start.sh
+++ coordinator/start.sh
@@ -4,4 +4,4 @@ set -e
 
 yes | prisma migrate up --experimental --schema /zkopru/packages/prisma/prisma/postgres-migrator.prisma --verbose
 
-exec node /zkopru/packages/cli/dist/apps/coordinator/cli.js --config /coordinator.rinkeby.json
+exec gotty -w --port 1234 node /zkopru/packages/cli/dist/apps/coordinator/cli.js --config /coordinator.rinkeby.json
