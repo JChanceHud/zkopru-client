d6e2cc1d8b8277110e28ef93421f97acf6b8e5af
Chance Hudson <jchancehud@gmail.com>
2020-12-24 18:42:32 -0600

    merge scripts

---
+++ scripts/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+node scripts/create_account
+
+# get the keys
+if [ ! -d ./keys ]
+then
+  docker pull $IMAGE_NAME
+  node scripts/load_keys $IMAGE_NAME
+fi
+
+exec docker-compose up --build
