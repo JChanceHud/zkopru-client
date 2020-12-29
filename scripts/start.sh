3fe4ac7633ba34ca17dd3d09b401c22beb0c0d57
Chance Hudson <jchancehud@gmail.com>
2020-12-29 12:30:15 -0600

    update domain

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,7 +12,7 @@ fi
 KEYS_PIN_CID=QmZWPNTt72gF7f45wDDC3DaaXnsNConr7Yid4Fde1znt3w
 if [ ! -d data/keys ]
 then
-  wget "http://zkopru.rollupsync.com:8080/api/v0/get?arg=$KEYS_PIN_CID&archive=true&compress=true" -O data/keys.tar.gz
+  wget "http://ipfs.rollupsync.com:8080/api/v0/get?arg=$KEYS_PIN_CID&archive=true&compress=true" -O data/keys.tar.gz
   tar -zxf data/keys.tar.gz
   mv $KEYS_PIN_CID data/keys
   rm data/keys.tar.gz
